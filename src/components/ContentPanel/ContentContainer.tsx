30cec22c002a89b2b93cf4261106ba3bc195a4f2
adinzlotyint <adrian.kamil.zietek@gmail.com>
2024-08-17 15:03:18 +0200

    Upgrades on ContentPanel

--- src/components/ContentPanel/ContentContainer.tsx
+++ src/components/ContentPanel/ContentContainer.tsx
@@ -1,11 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataTable from "./DataTable";
 import InputForm from "./InputForm";
 import { customData } from "./FormBody";
 
 const ContentContainer = () => {
-  const [data, setData] = useState<customData[]>([]); // Assuming data is an array of form submissions
+  const [data, setData] = useState<customData[]>(() => {
+    const storedData = localStorage.getItem("tableData");
+    return storedData ? JSON.parse(storedData) : [];
+  });
   const [fromScratch, setFromScratch] = useState(false); // Assuming data is an array of form submissions
+
+  const [deletingIndex, setDeletingIndex] = useState<number | null>(null);
+
+  const handleRowDelete = (index: number) => {
+    setDeletingIndex(index);
+    setTimeout(() => {
+      const newData = data.filter((item, i) => i !== index);
+      setData(newData);
+      setDeletingIndex(null);
+    }, 500); // Match this duration to your CSS transition
+  };
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("tableData");
+    if (storedData) {
+      try {
+        const parsedData = JSON.parse(storedData);
+        setData(parsedData);
+      } catch (error) {
+        console.error("Failed to parse data:", error);
+      }
+    }
+  }, []);
+
+  // Save data to Local Storage whenever it changes
+  useEffect(() => {
+    console.log("Data updated:", data);
+    localStorage.setItem("tableData", JSON.stringify(data));
+  }, [data]);
 
   return (
     <div className="grid grid-cols-1 grid-rows-layoutContentPanel p-6 bg-white xl:ml-4 ml-8 mr-8 rounded-2xl shadow-lg hover:shadow-xl transition-all h-full">
@@ -17,7 +49,12 @@ const ContentContainer = () => {
       </p>
 
       <InputForm setData={setData} />
-      <DataTable data={data} fromScratch={fromScratch} />
+      <DataTable
+        data={data}
+        fromScratch={fromScratch}
+        handleRowDelete={handleRowDelete}
+        deletingIndex={deletingIndex}
+      />
       <div className="flex justify-center">
         <button className="btn btn-neutral bg-primary hover:bg-primary shadow-lg rounded-lg w-1/4 min-h-8 h-6 font-roboto font-normal text-white">
           Download to file
--- src/components/ContentPanel/DataTable.tsx
+++ src/components/ContentPanel/DataTable.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { customData } from "./FormBody";
 import { FaTrashAlt } from "react-icons/fa";
+import "../../index.css";
 
 interface DataTableProps {
   data: customData[];
   fromScratch: boolean;
+  handleRowDelete: (index: number) => void;
+  deletingIndex: number | null;
 }
 
 const DataTable: React.FC<DataTableProps> = ({
   data,
-  fromScratch,
+  handleRowDelete,
+  deletingIndex,
 }: DataTableProps) => {
   return (
-    <div className="border border-spacing-1 rounded-xl mb-3 overflow-auto">
+    <div className="border border-spacing-1 rounded-lg mb-3 overflow-auto">
       <table className="table table-fixed font-roboto w-full">
         <thead>
           <tr>
@@ -21,12 +25,15 @@ const DataTable: React.FC<DataTableProps> = ({
             <th className="w-2/12 min-w-6">Consumption</th>
             <th className="w-1/12 min-w-6">Unit</th>
             <th className="w-auto min-w-6">Data Source</th>
-            <th className="w-1/12 min-w-6"></th>
+            <th className="w-[5%] min-w-6 px-2"></th>
           </tr>
         </thead>
         <tbody className="text-xs">
           {data.map((item, index) => (
-            <tr key={index}>
+            <tr
+              key={index}
+              className={deletingIndex === index ? "fade-out" : ""}
+            >
               <td className="break-words">{item.activity}</td>
               <td className="break-words">{item.emissionSource}</td>
               <td className="break-words">{item.consumption}</td>
@@ -34,8 +41,8 @@ const DataTable: React.FC<DataTableProps> = ({
               <td className="break-words whitespace-normal">
                 {item.dataSource}
               </td>
-              <td className="text-end">
-                <button className="btn-xs">
+              <td className="pr-1 pl-0 align-middle text-center">
+                <button onClick={() => handleRowDelete(index)}>
                   <FaTrashAlt className="text-lg" />
                 </button>
               </td>
